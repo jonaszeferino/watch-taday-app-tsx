f0837561e2fbc0799f93efc62bc7e1cb6de9e6bd
jonaszeferino <jonaszeferino@gmail.com>
2023-10-06 01:26:11 -0300

    first commit

---
+++ pages/api/getWeekendMovies.tsx
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const urlString =
+    "https://api.themoviedb.org/3/trending/movie/week?api_key=dd10bb2fbc12dfb629a0cbaa3f47810c";
+
+  try {
+    const response = await fetch(urlString, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status === 200) {
+      const result = await response.json();
+      res.status(200).json(result);
+    } else {
+      throw new Error("Dados Incorretos");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao buscar dados" });
+  }
+};
